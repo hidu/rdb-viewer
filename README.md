277d5dda8258b115b8eef2d29aa82ed28f4b8ef0
hidu <duv123+git@gmail.com>
2015-12-04 20:46:14 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # rdb-viewer
+parse redis rdb file
+
+
+## install
+```
+go install github.com/hidu/rdb-viewer
+```
+
+## useage
+```
+$ rdb-viewer dump.rdb
+hset    "xxxxx1" 6
+hset    "yyyyy2" 7
+```
+
+输出 3列: [string "abc" 12] 对应为 [类型 key 内容长度],eg  
+
+
+* 只输出string类型的 *：  
+```
+ $ rdb_viewer -types string part1.rdb
+```
+
+输出string和set，而且输出具体内容  
+```
+$ rdb_viewer -types string,set -val part1.rdb
+string  "xxx:phone:xxx:code"      7       value:  "verifed"       expiry: 0
+string  "xxx:sms:counter"  1       value:  "1"     expiry: 0
+```
+
+如上 第三列以后是 key： value 格式  
+
+
---
+++ rdb_viewer.go
@@ -0,0 +1,168 @@
+// This is a very basic example of a program that implements rdb.decoder and
+// outputs a human readable diffable dump of the rdb file.
+// copy from github.com/cupcake/rdb/examples/diff.go
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/cupcake/rdb"
+	"github.com/cupcake/rdb/nopdecoder"
+	"os"
+	"strings"
+)
+
+const (
+	kTypeString    string = "string"
+	kTypeHset             = "hset"
+	kTypeSet              = "set"
+	kTypeList             = "list"
+	kTypeSortedSet        = "sortedset"
+)
+
+var allKeyTypes = map[string]int{
+	kTypeString:    1,
+	kTypeHset:      1,
+	kTypeSet:       1,
+	kTypeList:      1,
+	kTypeSortedSet: 1,
+}
+
+var kTypePrint = map[string]bool{}
+
+var printValue = false
+
+type decoder struct {
+	db int
+	i  int
+	nopdecoder.NopDecoder
+}
+
+func (p *decoder) StartDatabase(n int) {
+	p.db = n
+}
+
+func (p *decoder) Set(key, value []byte, expiry int64) {
+	if kTypePrint[kTypeString] {
+		if printValue {
+			fmt.Printf("%s\t%q\t%d\tvalue:\t%q\texpiry:\t%d\n", kTypeString, key, len(value), value, expiry)
+		} else {
+			fmt.Printf("%s\t%q\t%d\n", kTypeString, key, len(value))
+		}
+	}
+}
+
+func (p *decoder) Hset(key, field, value []byte) {
+	if kTypePrint[kTypeHset] {
+		if printValue {
+			fmt.Printf("%s\t%q\t%d\tfield:\t%q\tvalue:\t%q\t\n", kTypeHset, key, len(field)+len(value), field, value)
+		} else {
+			fmt.Printf("%s\t%q\t%d\n", kTypeHset, key, len(field)+len(value))
+		}
+	}
+}
+
+func (p *decoder) Sadd(key, member []byte) {
+	if kTypePrint[kTypeSet] {
+		if printValue {
+			fmt.Printf("%s\t%q\t%d\tmember:\t%q\n", kTypeSet, key, len(member), member)
+		} else {
+			fmt.Printf("%s\t%q\t%d\n", kTypeSet, key, len(member))
+		}
+	}
+}
+
+func (p *decoder) StartList(key []byte, length, expiry int64) {
+	p.i = 0
+}
+
+func (p *decoder) Rpush(key, value []byte) {
+	if kTypePrint[kTypeList] {
+		if printValue {
+			fmt.Printf("%s\t%q\t%d\tvalue:\t%q\n", kTypeList, key, len(value), value)
+		} else {
+			fmt.Printf("%s\t%q\t%d\n", kTypeList, key, len(value))
+		}
+	}
+	p.i++
+}
+
+func (p *decoder) StartZSet(key []byte, cardinality, expiry int64) {
+	p.i = 0
+}
+
+func (p *decoder) Zadd(key []byte, score float64, member []byte) {
+	if kTypePrint[kTypeSortedSet] {
+		if printValue {
+			fmt.Printf("%s\t%s\t%d\tscore:\t%f\tmember:\t%q\n", kTypeSortedSet, string(key), len(member), score, member)
+		} else {
+			fmt.Printf("%s\t%s\t%d\n", kTypeSortedSet, string(key), len(member))
+		}
+	}
+	p.i++
+}
+
+func maybeFatal(err error) {
+	if err != nil {
+		fmt.Printf("Fatal error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+var printTypes string
+
+var typesAll []string
+
+var version = "0.2 20151204"
+
+func init() {
+	for kt := range allKeyTypes {
+		kTypePrint[kt] = false
+		typesAll = append(typesAll, kt)
+	}
+	flag.StringVar(&printTypes, "types", "", "print these types:["+strings.Join(typesAll, ",")+"],default all")
+	flag.BoolVar(&printValue, "val", false, "show values")
+	df := flag.Usage
+	flag.Usage = func() {
+		df()
+		name := os.Args[0]
+		fmt.Println("\n redis rdb viewer")
+		fmt.Println(" " + name + " -types string part1.rdb")
+		fmt.Println(" " + name + " -types string,set -val part1.rdb")
+		fmt.Println("")
+		fmt.Println(` first three parts : [string "abc" 12] -->[type key value_len]`)
+		fmt.Println("\n site: https://github.com/hidu/rdb-viewer")
+		fmt.Println(" version:", version)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	fileName := flag.Arg(0)
+
+	if fileName == "" {
+		fmt.Println("rdb file name required")
+		os.Exit(2)
+	}
+	parseArgTypes()
+
+	f, err := os.Open(fileName)
+	maybeFatal(err)
+	err = rdb.Decode(f, &decoder{})
+	maybeFatal(err)
+}
+
+func parseArgTypes() {
+	if printTypes == "" {
+		printTypes = strings.Join(typesAll, ",")
+	}
+	arr := strings.Split(printTypes, ",")
+	for _, v := range arr {
+		v = strings.TrimSpace(v)
+		if _, has := allKeyTypes[v]; has {
+			kTypePrint[v] = true
+		}
+	}
+}
